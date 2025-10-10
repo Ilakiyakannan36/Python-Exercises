63a014c91360241d7e667ae50a250ba29fc9c347
ILAKIYA KANNAN <ilakiyakannan36@gmail.com>
2025-10-10 15:38:25 +0530

    24/09/2025

--- game.py
+++ game.py
@@ -1,17 +1,16 @@
+# to guess a number and finding whether it is equal to the random number
 import random
 
-secret_number=random.randint(1,20)
-print(" Guess the number ")
+secret_number = random.randint(1, 20)
+print("***Number Guess***")
+
 while True:
-    guess = int(input("Guessed value="))
-    if guess < secret_number:
-        print("The value is low")
-        print("Try again")
-    elif guess >secret_number:
-        print("The value is high")
-        print("Try again")
-        
-    else :
-        print("You got it")
-        print("Correct answer")
-        break
+        guess = int(input("Take a guess: "))
+
+        if guess < secret_number:
+                print("The guess is low... higher number... Try again.")
+        elif guess > secret_number:
+                print("The guess is higher... lower number...Try again.")
+        else:
+                print(f"Congratulations! You guessed it right. The number was {secret_number}.")
+                break
