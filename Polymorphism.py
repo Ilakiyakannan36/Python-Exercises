3d295e669e5bdd5ed41b1a15384bad57c3fe2b80
ILAKIYA KANNAN <ilakiyakannan36@gmail.com>
2025-10-10 15:33:30 +0530

    10/10/2025

---
+++ Polymorphism.py
@@ -0,0 +1,16 @@
+"""class addition:
+    def add(a,b):
+        print(a+b)
+    add('muthu','ganesh')
+    add(2,3)
+    add(2.4,3.5)"""
+
+class A:
+    def greet(self):
+        print("A class")
+class B:
+    def disp(self):
+        print("B class")
+if __name__ == "__main__":
+    A()
+    
